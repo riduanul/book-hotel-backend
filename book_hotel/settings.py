3d21495af95ef4282b89123238edf51de3e92e21
riduanul <riduanul.haque1@gmail.com>
2024-02-05 15:44:35 +0600

    cors error problem solved

--- book_hotel/settings.py
+++ book_hotel/settings.py
@@ -16,7 +16,18 @@ SECRET_KEY = 'django-insecure-3eo20jv$ba8_k^3bw$9iev_d4k8nj867f_qw23h*xvy@g1k0cc
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://book-hotel.onrender.com/', 'https://*.127.0.0.1', 'https://book-hotel.onrender.com', 'https://book-hotel.onrender']
 
+# Application definition
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "https://book-hotel.onrender.com",
+    'https://*.127.0.0.1',
+    'http://localhost:5173/',
+ 
+]
 
 # Application definition
 
